0eaae6675a973e35578fe3248b0e5f77221782f8
bbwest0709 <bbwest0709@gmail.com>
2025-06-09 09:29:07 +0900

    setting

---
+++ north-shopping/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      메인화면
+    </div>
+  )
+}
+
+export default Home
---
+++ north-shopping/src/pages/Jewelery.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Jewelery = () => {
+  return (
+    <div className='container'>
+      주얼리
+    </div>
+  )
+}
+
+export default Jewelery
