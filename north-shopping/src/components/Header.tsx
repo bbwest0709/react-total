b1311bbec363215868a2ccb9d65eb079252d45cc
bbwest0709 <bbwest0709@gmail.com>
2025-06-09 09:52:37 +0900

    components add

---
+++ north-shopping/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="content-inner">
+            <div className="header-left">
+                로고, 메뉴
+            </div>
+            <div className="header-right">
+                로그인, 회원가입
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
